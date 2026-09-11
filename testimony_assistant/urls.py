f1d753cb3e3c8cbbfa2c858b04b1a1cc60be2800
douglasferreirag <douglas.ferreirag@gmail.com>
2026-09-11 20:52:46 -0300

    Template of registry of people

--- testimony_assistant/urls.py
+++ testimony_assistant/urls.py
@@ -3,21 +3,18 @@ URL configuration for testimony_assistant project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/6.1/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
 from django.urls import path
 
-from testimony.views import homepage, registry_of_letters, registry_of_letters_deliveries, registry_of_territories
+from testimony.views import (
+    homepage,
+    registry_of_letters,
+    registry_of_letters_deliveries,
+    registry_of_peoples,
+    registry_of_territories,
+    registry_of_peoples,   # nova view para pessoas
+)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -25,4 +22,5 @@ urlpatterns = [
     path('carta/registry/', registry_of_letters, name='registry_of_letters'),
     path('territorio/registry/', registry_of_territories, name='registry_of_territories'),
     path('entrega/registry/', registry_of_letters_deliveries, name='registry_of_letters_deliveries'),
+    path('pessoa/registry/', registry_of_peoples, name='registry_of_peoples'),  # nova rota
 ]
