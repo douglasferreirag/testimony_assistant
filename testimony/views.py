766ca275ab3e4cfc46762ccc8e663ae69da81554
douglasferreirag <douglas.ferreirag@gmail.com>
2026-09-12 23:31:28 -0300

    list of letters

--- testimony/views.py
+++ testimony/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
+# futuramente você pode importar o model Carta
+# from .models import Carta
 
 def homepage(request):
     return render(request, 'testimony/homepage.html')
@@ -17,3 +19,6 @@ def registry_of_peoples(request):
 
 def list_of_peoples(request):
     return render(request, 'testimony/list_of_peoples.html')
+
+def list_of_letters(request):
+    return render(request, 'testimony/list_of_letters.html')
--- testimony_assistant/urls.py
+++ testimony_assistant/urls.py
@@ -9,20 +9,21 @@ from django.urls import path
 
 from testimony.views import (
     homepage,
-    list_of_peoples,
     registry_of_letters,
     registry_of_letters_deliveries,
     registry_of_territories,
     registry_of_peoples,
-    list_of_peoples,   # nova view para listagem de pessoas
+    list_of_peoples,
+    list_of_letters,   # nova view para listagem de cartas
 )
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', homepage, name='homepage'),
     path('carta/registry/', registry_of_letters, name='registry_of_letters'),
+    path('carta/list/', list_of_letters, name='list_of_letters'),              # rota listagem de cartas
     path('territorio/registry/', registry_of_territories, name='registry_of_territories'),
     path('entrega/registry/', registry_of_letters_deliveries, name='registry_of_letters_deliveries'),
-    path('pessoa/registry/', registry_of_peoples, name='registry_of_peoples'),   # rota cadastro
-    path('pessoa/list/', list_of_peoples, name='list_of_peoples'),                      # rota listagem
+    path('pessoa/registry/', registry_of_peoples, name='registry_of_peoples'), # rota cadastro pessoas
+    path('pessoa/list/', list_of_peoples, name='list_of_peoples'),             # rota listagem pessoas
 ]
