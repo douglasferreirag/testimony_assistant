59cab994aeb087af8663ec60a4f3a0c407614988
douglasferreirag <douglas.ferreirag@gmail.com>
2026-09-17 21:41:32 -0300

    htmls

--- testimony/views.py
+++ testimony/views.py
@@ -5,67 +5,79 @@ from django.shortcuts import render
 def homepage(request):
     return render(request, 'testimony/homepage.html')
 
+# Letters
+
 def registry_of_letters(request):
-    return render(request, 'testimony/registry_of_letters.html')
-
-def registry_of_territories(request):
-    return render(request, 'testimony/registry_of_territories.html')
-
-def registry_of_letters_deliveries(request):
-    return render(request, 'testimony/registry_of_letters_deliveries.html')
-
-def registry_of_peoples(request):
-    return render(request, 'testimony/registry_of_peoples.html')
-
-def list_of_peoples(request):
-    return render(request, 'testimony/list_of_peoples.html')
+    return render(request, 'testimony/letters/registry_of_letters.html')
 
 def list_of_letters(request):
-    return render(request, 'testimony/list_of_letters.html')
+    return render(request, 'testimony/letters/list_of_letters.html')
+
+# Territories
+
+def registry_of_territories(request):
+    return render(request, 'testimony/territories/registry_of_territories.html')
 
 def list_of_territories(request):
-    return render(request, 'testimony/list_of_territories.html')
+    return render(request, 'testimony/territories/list_of_territories.html')
+
+# Peoples
+
+def registry_of_peoples(request):
+    return render(request, 'testimony/peoples/registry_of_peoples.html')
+
+def list_of_peoples(request):
+    return render(request, 'testimony/peoples/list_of_peoples.html')
+
+# Letters Deliveries
+
+def registry_of_letters_deliveries(request):
+    return render(request, 'testimony/letters_deliveries/registry_of_letters_deliveries.html')
 
 def list_of_letters_deliveries(request):
-    return render(request, 'testimony/list_of_letters_deliveries.html')
+    return render(request, 'testimony/letters_deliveries/list_of_letters_deliveries.html')
+
+# Conversations
 
 def registry_of_conversations(request):
-    return render(request, 'testimony/registry_of_conversations.html')
+    return render(request, 'testimony/conversations/registry_of_conversations.html')
 
 def list_of_conversations(request):
-    return render(request, 'testimony/list_of_conversations.html')
+    return render(request, 'testimony/conversations/list_of_conversations.html')
 
-# Revistas
+# Magazines
+
 def registry_of_magazines(request):
-    return render(request, 'testimony/registry_of_magazines.html')
+    return render(request, 'testimony/magazines/registry_of_magazines.html')
 
 def list_of_magazines(request):
-    return render(request, 'testimony/list_of_magazines.html')
+    return render(request, 'testimony/magazines/list_of_magazines.html')
 
-# Vídeos
+# Videos
+
 def registry_of_videos(request):
-    return render(request, 'testimony/registry_of_videos.html')
+    return render(request, 'testimony/videos/registry_of_videos.html')
 
 def list_of_videos(request):
-    return render(request, 'testimony/list_of_videos.html')
+    return render(request, 'testimony/videos/list_of_videos.html')
 
-# Livros
+# Books
 def registry_of_books(request):
-    return render(request, 'testimony/registry_of_books.html')
+    return render(request, 'testimony/books/registry_of_books.html')
 
 def list_of_books(request):
-    return render(request, 'testimony/list_of_books.html')
+    return render(request, 'testimony/books/list_of_books.html')
 
-# Folhetos (Leaflets)
+# Leaflets
 def registry_of_leaflets(request):
-    return render(request, 'testimony/registry_of_leaflets.html')
+    return render(request, 'testimony/leaflets/registry_of_leaflets.html')
 
 def list_of_leaflets(request):
-    return render(request, 'testimony/list_of_leaflets.html')
+    return render(request, 'testimony/leaflets/list_of_leaflets.html')
 
-# Estudos (Studies)
+# Studies
 def registry_of_studies(request):
-    return render(request, 'testimony/registry_of_studies.html')
+    return render(request, 'testimony/studies/registry_of_studies.html')
 
 def list_of_studies(request):
-    return render(request, 'testimony/list_of_studies.html')
+    return render(request, 'testimony/studies/list_of_studies.html')
